59eb5e672c0901c23d823b8cff67193397a12984
Loïc Séguin-C <lsc@loicseguin.com>
2017-10-08 12:55:24 -0400

    Working reusable plot with transitions.

--- xray/plot.js
+++ xray/plot.js
@@ -1,50 +1,121 @@
-function plot(x, y, svg) {
-    var margin = {top: 20, right: 20, bottom: 30, left: 70};
+function lineChart() {
+    var margin = {top: 20, right: 20, bottom: 30, left: 70},
+        width = 500,
+        height = 300,
+        xScale = d3.scaleLinear(),
+        yScale = d3.scaleLinear(),
+        xAxis = d3.axisBottom(xScale),
+        yAxis = d3.axisLeft(yScale),
+        colors = d3.scaleOrdinal(d3.schemeCategory10),
+        lines = []
+        g = null;
 
-    var width = 500;
-        height = 300;
+    function plot(selection) {
 
-    var xScale = d3.scale.linear()
-                   .domain(d3.extent(x))
-                   .range([0, width - margin.left - margin.right]);
-    var yScale = d3.scale.linear()
-                   .domain(d3.extent(y))
-                   .range([height - margin.top - margin.bottom, 0]);
+        selection.each(function(data) {
 
-    if (svg === undefined) {
-        var svg = d3.select("#figures")
-                    .append("svg")
-                    .attr("width", width)
-                    .attr("height", height)
-                    .append("g")
-                    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-        svg.append("g")
-             .attr("class", "x axis")
-             .attr("transform", "translate(0," + (height - margin.top - margin.bottom) + ")")
-             .call(d3.svg.axis().scale(xScale).orient("bottom"))
-             .select(".domain");
+            // Update the x scale
+            var domain = [
+                d3.min([xScale.domain()[0], d3.min(data, function(d) { return d[0]; })]),
+                d3.max([xScale.domain()[1], d3.max(data, function(d) { return d[0]; })])
+            ];
+            xScale
+                .domain(domain)
+                .range([0, width - margin.left - margin.right]);
 
-        svg.append("g")
-             .attr("class", "y axis")
-             .call(d3.svg.axis().scale(yScale).orient("left"));
-           //.append("text")
-             //.attr("class", "label")
-             //.attr("transform", "rotate(-90)")
-             //.attr("x", -height/2)
-             //.attr("y", -margin.left*0.8)
-             //.attr("dy", ".71em")
-             //.style("text-anchor", "middle")
-             //.text("Intensité relative");
+            // Update the y scale
+            domain = [
+                d3.min([yScale.domain()[0], d3.min(data, function(d) { return d[1]; })]),
+                d3.max([yScale.domain()[1], d3.max(data, function(d) { return d[1]; })])
+            ];
+            yScale
+                .domain(domain)
+                .range([height - margin.top - margin.bottom, 0]);
+
+            // Select the svg element if it exists.
+            var svg = d3.select(this).selectAll("svg").data([data]);
+            var svgEnter = svg.enter().append("svg");
+
+            // Otherwise create skeletal graph.
+            var gEnter = svgEnter.append("g");
+            gEnter.append("g").attr("class", "x axis");
+            gEnter.append("g").attr("class", "y axis");
+
+            // Update outer dimensions, ie, dimensions of the svg element.
+            svg = svg.merge(svgEnter);
+            svg.attr("width", width)
+                .attr("height", height);
+
+            // Update inner dimensions, ie, dimensions of g element.
+            g = svg.select("g")
+                .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+            // Update previous lines.
+            g.selectAll(".line").each(function (d, i) {
+                lines[i].x(function(d) { return xScale(d[0]); })
+                        .y(function(d) { return yScale(d[1]); });
+                d3.select(this).transition()
+                    .duration(1000)
+                    .attr("d", lines[i]);
+            });
+
+            // Add the line path.
+            var line = d3.line()
+                .x(function (d) { return xScale(d[0]); })
+                .y(function (d) { return yScale(d[1]); });
+            g.append("path")
+                .datum(data)
+                .attr("class", "line")
+                .attr("d", line)
+                .style("stroke", function() { return colors(lines.length); });
+            lines.push(line);
+
+            // Update the x-axis.
+            g.select(".x.axis")
+                .attr("transform", "translate(0," + yScale.range()[0] + ")")
+                .transition()
+                .duration(1000)
+                .call(xAxis);
+
+            // Update the y-axis.
+            g.select(".y.axis")
+                .transition()
+                .duration(1000)
+                .call(yAxis);
+        });
     }
 
-    var line = d3.svg.line()
-                 .x(function (d, i) { return xScale(d); })
-                 .y(function (d, i) { return yScale(y[i]); });
+    plot.xdomain = function(domain) {
+        xScale.domain(domain);
+        // Redraw the lines
+        g.selectAll(".line").each(function (d, i) {
+            lines[i].x(function(d) { return xScale(d[0]); });
+            d3.select(this).transition()
+                .duration(1000)
+                .attr("d", lines[i]);
+        });
+        // Update the x-axis.
+        g.select(".x.axis")
+            .transition()
+            .duration(1000)
+            .call(xAxis);
+    };
 
-    svg.append("path")
-        .data([x])
-        .attr("class", "line")
-        .attr("d", line);
+    plot.ydomain = function(domain) {
+        yScale.domain(domain);
+        // Redraw the lines
+        g.selectAll(".line").each(function (d, i) {
+            lines[i].y(function(d) { return yScale(d[1]); });
+            d3.select(this).transition()
+                .duration(1000)
+                .attr("d", lines[i]);
+        });
+        // Update the y-axis.
+        g.select(".y.axis")
+            .transition()
+            .duration(1000)
+            .call(yAxis);
+    };
 
-    return svg;
+    return plot;
 }
--- xray/xray.js
+++ xray/xray.js
@@ -1,9 +1,35 @@
+var nfigures = 0;
+
+function addFigure() {
+    var plot = lineChart()
+    d3.select("#figures")
+        .append("div")
+        .attr("id", "figure" + nfigures)
+        .data([data])
+        .call(plot);
+    nfigures++;
+
+    return plot;
+}
+
 var x = d3.range(0, 10, 0.1);
 var y = x.map(function (d) { return Math.sin(d); });
-fig = plot(x, y)
+data = d3.zip(x, y);
+var plot = addFigure();
 
-x = d3.range(0, 30, 0.1);
-y = x.map(function (d) { return Math.sin(d) + Math.cos(3*d) -2; });
-plot(x, y, fig);
-fig.selectAll("path")
-   .attr("style", "stroke: #333");
+d3.select("#changedomain").on("click", function() {
+    plot.xdomain([0, 40 * Math.random()]);
+    plot.ydomain([-15 * Math.random(), 15 * Math.random()]);
+});
+
+d3.select("#addplot").on("click", function() {
+    //plot.xdomain(0, 40 * Math.random());
+    x = d3.range(0, 30 * Math.random(), 0.1);
+    y = x.map(function(d) { return Math.cos(2*Math.random()*d) + 1.3*Math.random() * Math.sin(0.8*d); });
+    data = d3.zip(x, y);
+    d3.select("#figure0")
+        .data([data])
+        .call(plot);
+});
+
+d3.select("#addfigure").on("click", addFigure);
