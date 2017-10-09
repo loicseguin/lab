47fc104cbc53e79479e39fe3b3aa72382f684fc5
Loïc Séguin-C <lsc@loicseguin.com>
2017-10-09 11:03:01 -0400

    Start physics code, better graphs

--- xray/plot.js
+++ xray/plot.js
@@ -1,14 +1,22 @@
 function lineChart() {
-    var margin = {top: 20, right: 20, bottom: 30, left: 70},
-        width = 500,
-        height = 300,
+    var margin = {top: 20, right: 20, bottom: 40, left: 60},
+        width = 500 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom,
         xScale = d3.scaleLinear(),
         yScale = d3.scaleLinear(),
         xAxis = d3.axisBottom(xScale),
         yAxis = d3.axisLeft(yScale),
+        xGrid = d3.axisBottom(xScale)
+                    .tickSize(-height, 0, 0)
+                    .tickFormat(""),
+        yGrid = d3.axisLeft(yScale)
+                    .tickSize(-width, 0, 0)
+                    .tickFormat(""),
         colors = d3.scaleOrdinal(d3.schemeCategory10),
-        lines = []
-        g = null;
+        g = null,
+        x_axis_title = "",
+        y_axis_title = "",
+        nlines = 0;
 
     function plot(selection) {
 
@@ -21,7 +29,7 @@ function lineChart() {
             ];
             xScale
                 .domain(domain)
-                .range([0, width - margin.left - margin.right]);
+                .range([0, width]);
 
             // Update the y scale
             domain = [
@@ -30,7 +38,7 @@ function lineChart() {
             ];
             yScale
                 .domain(domain)
-                .range([height - margin.top - margin.bottom, 0]);
+                .range([height, 0]);
 
             // Select the svg element if it exists.
             var svg = d3.select(this).selectAll("svg").data([data]);
@@ -38,26 +46,22 @@ function lineChart() {
 
             // Otherwise create skeletal graph.
             var gEnter = svgEnter.append("g");
+            gEnter.append("g").attr("class", "x grid");
+            gEnter.append("g").attr("class", "y grid");
             gEnter.append("g").attr("class", "x axis");
             gEnter.append("g").attr("class", "y axis");
 
             // Update outer dimensions, ie, dimensions of the svg element.
             svg = svg.merge(svgEnter);
-            svg.attr("width", width)
-                .attr("height", height);
+            svg.attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom);
 
             // Update inner dimensions, ie, dimensions of g element.
             g = svg.select("g")
                 .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
             // Update previous lines.
-            g.selectAll(".line").each(function (d, i) {
-                lines[i].x(function(d) { return xScale(d[0]); })
-                        .y(function(d) { return yScale(d[1]); });
-                d3.select(this).transition()
-                    .duration(1000)
-                    .attr("d", lines[i]);
-            });
+            updateLines();
 
             // Add the line path.
             var line = d3.line()
@@ -67,54 +71,125 @@ function lineChart() {
                 .datum(data)
                 .attr("class", "line")
                 .attr("d", line)
-                .style("stroke", function() { return colors(lines.length); });
-            lines.push(line);
+                .style("stroke", function() { return colors(nlines); });
+            nlines++;
+
+            // Create grid lines.
+            var duration = 0;
+            if (nlines > 1) { duration = 1000; }
+            g.select(".x.grid")
+                .attr("transform", "translate(0," + height + ")")
+                .transition()
+                .duration(duration)
+                .call(xGrid);
+
+            g.select(".y.grid")
+                .transition()
+                .duration(duration)
+                .call(yGrid);
 
             // Update the x-axis.
             g.select(".x.axis")
                 .attr("transform", "translate(0," + yScale.range()[0] + ")")
                 .transition()
-                .duration(1000)
+                .duration(duration)
                 .call(xAxis);
+            g.select(".x.axis")
+                .selectAll(".x.label")
+                .data([x_axis_title])
+                .enter()
+                .append("text")
+                .attr("class", "x label")
+                .attr('x', width / 2)
+                .attr('y', 30)
+                .style('text-anchor', 'middle')
+                .text(function(d) { return d; });
 
             // Update the y-axis.
             g.select(".y.axis")
                 .transition()
-                .duration(1000)
+                .duration(duration)
                 .call(yAxis);
+            g.select(".y.axis")
+                .selectAll(".y.label")
+                .data([y_axis_title])
+                .enter()
+                .append("text")
+                .attr("class", "y label")
+                .attr("transform", "rotate(-90)")
+                .attr("x", -height/2)
+                .attr("y", -40)
+                .attr("dy", ".71em")
+                .style("text-anchor", "middle")
+                .text(function(d) { return d; });
+
+        });
+    }
+
+    function updateLines() {
+        g.selectAll(".line").each(function (d, i) {
+            var line = d3.line()
+                .x(function (d) { return xScale(d[0]); })
+                .y(function (d) { return yScale(d[1]); });
+            d3.select(this)
+                .transition()
+                .duration(1000)
+                .attr("d", line);
         });
     }
 
     plot.xdomain = function(domain) {
+        if (!arguments.length) {
+            return xScale.domain();
+        }
         xScale.domain(domain);
         // Redraw the lines
-        g.selectAll(".line").each(function (d, i) {
-            lines[i].x(function(d) { return xScale(d[0]); });
-            d3.select(this).transition()
-                .duration(1000)
-                .attr("d", lines[i]);
-        });
-        // Update the x-axis.
-        g.select(".x.axis")
-            .transition()
-            .duration(1000)
-            .call(xAxis);
+        if (g) {
+            updateLines();
+            // Update the x-axis.
+            g.select(".x.axis")
+                .call(xAxis);
+        }
+        return plot;
     };
 
     plot.ydomain = function(domain) {
+        if (!arguments.length) {
+            return yScale.domain();
+        }
         yScale.domain(domain);
         // Redraw the lines
-        g.selectAll(".line").each(function (d, i) {
-            lines[i].y(function(d) { return yScale(d[1]); });
-            d3.select(this).transition()
-                .duration(1000)
-                .attr("d", lines[i]);
-        });
-        // Update the y-axis.
-        g.select(".y.axis")
-            .transition()
-            .duration(1000)
-            .call(yAxis);
+        if (g) {
+            updateLines();
+            // Update the y-axis.
+            g.select(".y.axis")
+                .call(yAxis);
+        }
+        return plot;
+    };
+
+    plot.update = function(data) {
+        g.select(".line")
+            .datum(data)
+            .attr("d", d3.line()
+                .x(function(d) { return xScale(d[0]); })
+                .y(function(d) { return yScale(d[1]); }));
+    };
+
+    plot.xAxisLabel = function(val) {
+        if (!arguments.length) {
+            return x_axis_title;
+        }
+        x_axis_title = val;
+        return plot;
+    };
+
+    plot.yAxisLabel = function(val) {
+        if (!arguments.length) {
+            return y_axis_title;
+        }
+        y_axis_title = val;
+        return plot;
     };
 
     return plot;
--- xray/xray.js
+++ xray/xray.js
@@ -1,35 +1,127 @@
-var nfigures = 0;
+function normalPDF(x, mu, sigma) {
+    if (mu === undefined) { mu = 0; }
+    if (sigma === undefined) { sigma = 1; }
 
-function addFigure() {
-    var plot = lineChart()
-    d3.select("#figures")
-        .append("div")
-        .attr("id", "figure" + nfigures)
-        .data([data])
-        .call(plot);
-    nfigures++;
-
-    return plot;
+    var norm = 1 / (Math.sqrt(2 * Math.PI) * sigma);
+    return norm * Math.exp(-((x - mu)**2) / (2 * sigma**2));
 }
 
-var x = d3.range(0, 10, 0.1);
-var y = x.map(function (d) { return Math.sin(d); });
-data = d3.zip(x, y);
-var plot = addFigure();
+function characteristicSpectrum(photonEnergy) {
+    // Characteristic spectrum of tungsten.
+    var lineEnergy =
+        [0.08, 0.52, 0.6, 2.2, 2.7, 2.8, 9.3, 11.5, 12, 12.1, 57.4,
+         66.7, 68.9, 69.4, 69.5];
+    var lineWidth =
+        [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+         0.5, 0.5, 0.5];
+    var relativeIntensity =
+        [0.2, 0.1, 0.2, 0.2, 0.5, 0.5, 1, 1, 1, 1, 1.5, 1, 0.4,
+         2, 0.5];
 
-d3.select("#changedomain").on("click", function() {
-    plot.xdomain([0, 40 * Math.random()]);
-    plot.ydomain([-15 * Math.random(), 15 * Math.random()]);
-});
+    var lineInfo = d3.zip(lineEnergy, lineWidth, relativeIntensity);
+
+    var nbPhotons = photonEnergy.map(function(d) {
+        return d3.sum(lineInfo.map(function(info) {
+            return info[2] * normalPDF(d, info[0], info[1] / 4);
+        }));
+    });
+    return nbPhotons;
+}
+
+function bremsstrahlungSpectrum(photonEnergy, n0, noiseSD) {
+    if (n0 === undefined) { n0 = 100; }
+    if (noiseSD === undefined) { noiseSD = 2; }
+    var tubePotential = photonEnergy[photonEnergy.length - 1];
+
+    // Number of photons with some random noise.
+    var slope = -n0 / tubePotential;
+    var nbPhotons = photonEnergy.map(function(d) {
+        var noise = d3.randomNormal(0, noiseSD)();
+        return n0 + slope * d + noise; 
+    });
+
+    return nbPhotons;
+}
+
+function genSpectrum(photonEnergy, n0, noiseSD) {
+    if (n0 === undefined) { n0 = 100; }
+    if (noiseSD === undefined) { noiseSD = 2; }
+
+    var nbPhotons = bremsstrahlungSpectrum(photonEnergy, n0);
+    var charSpec = characteristicSpectrum(photonEnergy);
+    nbPhotons = nbPhotons.map(function(n, i) {
+        return n + n / 10 * charSpec[i];
+    });
+    return d3.zip(photonEnergy, nbPhotons);
+}
+
+function addFigure(data) {
+}
+
+var tubePotential = [40, 55, 80, 100],
+    n0s = [200, 100, 50, 300];
+
+var datas = d3.zip(tubePotential, n0s)
+    .map(function(d) {
+        var photonEnergy = d3.range(0, d[0], 0.1);
+        return genSpectrum(photonEnergy, d[1]);
+    });
+
+var nplots = 0;
+var plot0 = lineChart()
+    .xAxisLabel("Énergie des photons (keV)")
+    .yAxisLabel("Nombre de photons");
+d3.select("#figures")
+    .append("div")
+    .attr("id", "figure0")
+    .data([datas[0]])
+    .call(plot0);
+nplots++;
 
 d3.select("#addplot").on("click", function() {
-    //plot.xdomain(0, 40 * Math.random());
-    x = d3.range(0, 30 * Math.random(), 0.1);
-    y = x.map(function(d) { return Math.cos(2*Math.random()*d) + 1.3*Math.random() * Math.sin(0.8*d); });
-    data = d3.zip(x, y);
-    d3.select("#figure0")
-        .data([data])
-        .call(plot);
+    if (nplots < datas.length) {
+        d3.select("#figure0")
+            .data([datas[nplots]])
+            .call(plot0);
+        nplots++;
+    }
+    if (nplots >= datas.length) {
+        d3.select(this).attr("disabled", "disabled");
+    }
 });
 
-d3.select("#addfigure").on("click", addFigure);
+
+var photonEnergy = d3.range(0, 50, 0.1);
+var n0 = 50;
+var plot1 = lineChart()
+    .xAxisLabel("Énergie des photons (keV)")
+    .yAxisLabel("Nombre de photons")
+    .xdomain([0, 100])
+    .ydomain([0, 100]);
+d3.select("#figures")
+    .append("div")
+    .attr("id", "figure1")
+    .data([genSpectrum(photonEnergy, n0)])
+    .call(plot1);
+d3.select("#figure1")
+    .append("input")
+    .attr("type", "range")
+    .attr("min", 0)
+    .attr("max", 100)
+    .attr("id", "tubePotential")
+    .on("input", function() {
+        photonEnergy = d3.range(0, +this.value, 0.1);
+        data = genSpectrum(photonEnergy, n0);
+        plot1.update(data);
+    });
+d3.select("#figure1")
+    .append("input")
+    .attr("type", "range")
+    .attr("min", 0)
+    .attr("max", 100)
+    .attr("id", "n0")
+    .on("input", function() {
+        n0 = +this.value;
+        data = genSpectrum(photonEnergy, n0);
+        plot1.update(data);
+    });
